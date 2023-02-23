92544dd4f1e25e46eb7a9c4ded31706666eacdbf
JooYoung2274 <jooooo2274@gmail.com>
2023-02-23 11:31:54 +0900

    [update] query test

--- src/query/query.controller.ts
+++ src/query/query.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { QueryService } from './query.service';
 
 @Controller('query')
-export class QueryController {}
+export class QueryController {
+  constructor(private queryService: QueryService) {}
+  @Get('/')
+  async test(@Body() body) {
+    return await this.queryService.test(body);
+  }
+}
