ee5b274de82b8a05db372b3169cf4f9ba51f4d23
JooYoung2274 <jooooo2274@gmail.com>
2023-02-23 09:44:07 +0900

    [init] mysql2 connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { ConnectionService } from './connection.service';
+import { QueryModule } from './query/query.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [QueryModule],
+  controllers: [],
+  providers: [ConnectionService],
 })
 export class AppModule {}
---
+++ src/connection.service.ts
@@ -0,0 +1,21 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import * as mysql from 'mysql2/promise';
+
+@Injectable()
+export class ConnectionService implements OnModuleInit {
+  constructor() {}
+
+  async onModuleInit() {
+    const pool = mysql.createPool({
+      host: process.env.DB_HOST,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: 3306,
+      database: process.env.DB_DATABASE,
+      connectionLimit: 50,
+    });
+
+    console.log(`✅ START CONNECTION 🚀 `);
+    return pool;
+  }
+}
