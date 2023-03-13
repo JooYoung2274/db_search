af8cd261615cfd77e6e51678e939e87fc3b33f0a
JooYoung2274 <jooooo2274@gmail.com>
2023-03-13 09:16:58 +0900

    [update] token api

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { IAuthRepository } from './auth.interface';
+import { AuthRepository } from './auth.repository';
+import { JwtStrategy } from './passport/jwt.strategy';
+import { JwtRefreshTokenStrategy } from './passport/jwtRefresh.strategy';
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+    imports: [
+        PassportModule,
+        JwtModule.register({
+            secret: process.env.JWT_SECRET,
+            signOptions: { expiresIn: process.env.JWT_EXPIRE_TIME },
+        }),
+    ],
+    controllers: [AuthController],
+    providers: [
+        AuthService,
+        {
+            provide: IAuthRepository,
+            useClass: AuthRepository,
+        },
+        JwtStrategy,
+        JwtRefreshTokenStrategy,
+    ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { IAuthRepository } from './auth.interface';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(
+        @Inject(IAuthRepository) private authRepository: IAuthRepository,
+        private readonly jwtService: JwtService,
+    ) {}
+    async getUserIfRefreshTokenMatches(refreshToken: string, userId: number): Promise<any> {
+        const userRefreshToken = await this.authRepository.getResfreshTokenByUserId(userId);
+
+        if (refreshToken === userRefreshToken) {
+            const newAccessToken = await this.jwtGenerate(userId, 'accessToken');
+            return { accessToken: newAccessToken };
+        }
+        return false;
+    }
+
+    async jwtGenerate(userId: number, key: string) {
+        let jwtExpireTime;
+        let jwtSecretKey;
+
+        if (key === 'accessToken') {
+            jwtExpireTime = process.env.JWT_EXPIRE_TIME;
+            jwtSecretKey = process.env.JWT_SECRET;
+        }
+        if (key === 'refreshToken') {
+            jwtExpireTime = process.env.JWT_REFRESH_EXPIRE_TIME;
+            jwtSecretKey = process.env.JWT_REFRESH_SECRET;
+        }
+        return {
+            accessToken: this.jwtService.sign(
+                {
+                    userId: userId,
+                },
+                {
+                    secret: jwtSecretKey,
+                    expiresIn: jwtExpireTime,
+                },
+            ),
+            userId: userId,
+        };
+    }
+}
