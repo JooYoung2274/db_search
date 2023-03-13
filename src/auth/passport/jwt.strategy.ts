c0430bed5faf24539a452e99d81ee6e5e01774c0
JooYoung2274 <jooooo2274@gmail.com>
2023-03-13 09:16:38 +0900

    [update] passport strategy

---
+++ src/auth/passport/jwt.strategy.ts
@@ -0,0 +1,31 @@
+/*
+  ==============================================================================
+    (c) 2022. quantum universe All rights reserved.
+    author : JOOYOUNG KIM
+    start date : 11/08/2022
+  ==============================================================================
+*/
+import { Request } from 'express';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromExtractors([
+                (request: Request) => {
+                    return request?.cookies?.ACCESS_TOKEN;
+                },
+            ]),
+            secretOrKey: process.env.JWT_SECRET,
+            passReqToCallback: true,
+        });
+    }
+
+    async validate(request: Request, payload: any) {
+        const accessToken = request.cookies?.ACCESS_TOKEN;
+        return payload;
+    }
+}
