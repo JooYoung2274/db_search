79176ee2aba832b389c0db75959dd8bd61a24eb5
JooYoung2274 <jooooo2274@gmail.com>
2023-03-14 09:42:31 +0900

    [update] 직렬화 로직 추가

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
+import cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.use(cookieParser());
 
-  app.setBaseViewsDir(join(__dirname, '..', 'views'));
-  app.setViewEngine('hbs');
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  await app.listen(3000);
+    app.setBaseViewsDir(join(__dirname, '..', 'views'));
+    app.setViewEngine('hbs');
+
+    await app.listen(3000, () => {
+        console.log('서버시작');
+    });
 }
 bootstrap();
